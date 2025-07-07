2d59e15ba91d5af30450babec28b7d7645f9d489
mrbuche <dev@mrbuche.com>
2025-07-07 17:22:05 -0600

    update

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,12 +1,10 @@
-using Aqua, Conspire, JET, JuliaFormatter, LinearAlgebra, Test
+using Aqua, Conspire, JuliaFormatter, LinearAlgebra, Test
 
 ϵ = 1e-6
 
 include("constitutive.jl")
 
 Aqua.test_all(Conspire)
-
-println("\nJET:\n", report_package("Conspire"; toplevel_logger = nothing))
 
 if !format("../", overwrite = false, verbose = true)
     error("File(s) not properly formatted.")
